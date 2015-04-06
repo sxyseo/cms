e99432628cc45955428837d85eb5308e9bc3d1a9
calm04061 <calm0406@163.com>
2015-04-06 16:15:25 +0800

    添加事务管理

--- cms-api/src/main/java/com/calm/cms/api/service/ITableColumnService.java
+++ cms-api/src/main/java/com/calm/cms/api/service/ITableColumnService.java
@@ -1,9 +1,11 @@
 package com.calm.cms.api.service;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.calm.cms.api.entity.TableColumn;
 import com.calm.cms.api.entity.TableColumnKey;
 import com.calm.framework.common.service.IBaseService;
-
+@Transactional
 public interface ITableColumnService extends IBaseService<TableColumnKey,TableColumn> {
 
 	/**
